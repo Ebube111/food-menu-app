69d50cb98fa78d243cbcc00b9ba73539bea42caa
Ebube111 <ebubeagwaze@gmail.com>
2021-03-08 11:43:18 +0100

    my first commit

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const OrderDelivery = () => {
+    return (
+        <View>
+            <Text>Welcome OrderDelivery</Text>
+        </View>
+    )
+}
+
+export default OrderDelivery;
